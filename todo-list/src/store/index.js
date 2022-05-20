482dc0419d723ad2e090bbc71deef460fab2dc2a
Dailenis Z. Zorrilla <dzorrilla88@gmail.com>
2022-05-20 03:39:40 +0200

    no api rest service at the moment

--- todo-list/src/store/index.js
+++ todo-list/src/store/index.js
@@ -52,36 +52,8 @@ const store = new Vuex.Store({
         clearCompleted({state, commit}){
             let list = state.todoList.filter(ele => ele.state !== 'completed'  )
             commit('setTodoList', list)
-        },
-
-        // CRUD REPRESENTATION USIN AXIOS 
-
-           /** Get all todo from API*/ 
-            async listTodo( {commit}){    
-              const data = await makeRequest('todo')
-              commit('setTodoList', data);
-            },
-
-            /** Create a new TODO */
-            async postTodo(  data){
-                const datas = await makeRequest('todo', 'post', data)
-                this.addTodoItem(datas) 
-                return datas;  
-            },
-
-          /** Update a Todo */
-          async putTodo( index, data){
-              const datas = await makeRequest(`todo/${data.id}`, 'put', data)
-              this.updateTodoItem(index, datas)
-              return datas; 
-          },
-
-          /** Delete a todo */
-          async deleteTodo(  id){  
-              const response = await makeRequest(`todo/${id}`, 'delete')
-              if(response) this.removeTodoItem(id)
-              return true; 
-          },
+        }
+       
     },
   });
 
